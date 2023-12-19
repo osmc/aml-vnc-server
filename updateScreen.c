e70c8c22a1d0c4d061925eae23b7b8ce6f8d2a1b
dtechsrv <dee.gabor@gmail.com>
2023-12-19 13:15:55 +0100

    Rework image buffer comparison solution
    - Fix comparison grid distance for lower resolutions,
    - Break the comparison in each line when it detects the first difference,
    - Modify the static matrix to a dynamic offset grid solution,
    - Add a random slip to eliminate any remaining dirty zones,
    - Clean up the code and add some comments.

--- updateScreen.c
+++ updateScreen.c
@@ -3,6 +3,7 @@ droid VNC server  - a vnc server for android
 Copyright (C) 2011 Jose Pereira <onaips@gmail.com>
 
 Modified for AML TV Boxes by kszaq <kszaquitto@gmail.com>
+Additional developments by dtechsrv <dee.gabor@gmail.com>
 
 This library is free software; you can redistribute it and/or
 modify it under the terms of the GNU Lesser General Public
@@ -21,45 +22,74 @@ Foundation, Inc., 51 Franklin Street, Fifth Floor, Boston, MA  02110-1301  USA
 
 #define OUT_T CONCAT3E(uint,OUT,_t)
 #define FUNCTION CONCAT2E(update_screen_,OUT)
-#define COMPRECT 4
 #define MIN(a,b) (((a)<(b))?(a):(b))
 #define MAX(a,b) (((a)>(b))?(a):(b))
+#define SQUARE(x) ((x)*(x))
 
 void FUNCTION(void) {
-	OUT_T* b = (OUT_T*)readBufferFB();
-	OUT_T* a = (OUT_T*)vncbuf;
+	idle = 1; // Reset idle state first
 
-	struct fb_var_screeninfo scrinfo; //we'll need this to detect double FB on framebuffer
+	OUT_T* fb = (OUT_T*)readBufferFB();
+	OUT_T* vb = (OUT_T*)vncbuf;
+
+	// Get screen information
+	struct fb_var_screeninfo scrinfo;
 	scrinfo = FB_getscrinfo();
 
-	idle = 1;
-
-	int i, j;
-	int offset = 0, fb_offset = 0;
+	int i, j, gridslip, px_step, px_random;
+	int vb_offset = 0, fb_offset = 0, px_offset = 0;
 	int max_x = -1, max_y = -1, min_x = 99999, min_y = 99999;
 
-	for (j = 0; j < vncscr->height; j+=COMPRECT) {
-		offset = j * vncscr->width;
+	// Set the pixel grid slip distance (depends on the screen resolution)
+	if (vncscr->height <= 540) {
+		gridslip = 2;
+	} else if (vncscr->height > 540 && vncscr->height < 1080) {
+		gridslip = 3;
+	} else {
+		gridslip = 4;
+	}
+
+	// Set the inline pixel step distance
+	px_step = SQUARE(gridslip) - 1;
+
+	// Generate a random step offset (It helps to eliminate any remaining dirty zones between each image update.)
+	srand(time(NULL));
+	px_random = rand() % px_step;
+
+	// Compare the buffers and find the differences in every line
+	for (j = 0; j < vncscr->height; j++) {
+
+		// Set all offsets
+		vb_offset = j * vncscr->width;
 		fb_offset = (j + scrinfo.yoffset) * scrinfo.xres_virtual + scrinfo.xoffset;
-		for (i = 0; i < vncscr->width; i+=COMPRECT) {
-			if (a[i + offset] != b[i + fb_offset]) {
-				min_y=MIN(j - COMPRECT, min_y);
-				max_y=MAX(j + COMPRECT, max_y);
-				idle = 0;
+		px_offset = (j * gridslip + px_random) % px_step;
+
+		// Compare certain pixels in this line with an offset
+		for (i = px_offset; i < vncscr->width; i += px_step) {
+			if (vb[i + vb_offset] != fb[i + fb_offset]) {
+				if (idle) {
+					idle = 0;
+					min_y = MIN(j - gridslip, min_y); // Set the current line as the first
+				}
+
+				// Set the current line as the last
+				max_y = MAX(j + gridslip, max_y);
+				break; // There is no need to examine this line anymore if it already has a difference
 			}
 		}
 	}
 
 	if (!idle) {
-		min_x=0;
-		min_y=MAX(0, min_y);
-		max_x=screenformat.width - 1;
-		max_y=MIN(screenformat.height - 1, max_y);
+		min_x = 0;
+		min_y = MAX(0, min_y);
+		max_x = screenformat.width - 1;
+		max_y = MIN(screenformat.height - 1, max_y);
 
+		// Fill the image buffer with the new content
 		for (j = min_y; j <= max_y; j++) {
-			offset = j * vncscr->width;
+			vb_offset = j * vncscr->width;
 			fb_offset = (j + scrinfo.yoffset) * scrinfo.xres_virtual + scrinfo.xoffset;
-			memcpy(vncbuf + offset, b + fb_offset, screenformat.width * screenformat.bitsPerPixel / CHAR_BIT);
+			memcpy(vncbuf + vb_offset, fb + fb_offset, screenformat.width * screenformat.bitsPerPixel / CHAR_BIT);
 		}
 
 		rfbMarkRectAsModified(vncscr, min_x, min_y, max_x, max_y);
