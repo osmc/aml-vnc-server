ffdb53864ec50a078fb7bbd8522089c0b1945657
dtechsrv <dee.gabor@gmail.com>
2023-12-25 17:20:06 +0100

    Finalize and cleanup screen update code

--- updateScreen.c
+++ updateScreen.c
@@ -27,8 +27,14 @@ Foundation, Inc., 51 Franklin Street, Fifth Floor, Boston, MA  02110-1301  USA
 #define SQUARE(x) ((x)*(x))
 
 void FUNCTION(void) {
-	idle = 1; // Reset idle state first
+	int x, y, width, height, slip, step, shift;
+	int vb_offset = 0, fb_offset = 0, px_offset = 0;
+	int max_x = -1, max_y = -1, min_x = 99999, min_y = 99999;
 
+	// Reset idle state first
+	idle = 1;
+
+	// Create buffers
 	OUT_T* fb = (OUT_T*)readBufferFB();
 	OUT_T* vb = (OUT_T*)vncbuf;
 
@@ -36,59 +42,59 @@ void FUNCTION(void) {
 	struct fb_var_screeninfo scrinfo;
 	scrinfo = FB_getscrinfo();
 
-	int i, j, gridslip, px_step, px_random;
-	int vb_offset = 0, fb_offset = 0, px_offset = 0;
-	int max_x = -1, max_y = -1, min_x = 99999, min_y = 99999;
+	// Screen resolution
+	width = vncscr->width;
+	height = vncscr->height;
 
-	// Set the pixel grid slip distance (depends on the screen resolution)
-	if (vncscr->height <= 540) {
-		gridslip = 2;
-	} else if (vncscr->height > 540 && vncscr->height < 1080) {
-		gridslip = 3;
+	// Set the pixel grid slip (depends on the resolution)
+	if (height <= 540) {
+		slip = 2;
+	} else if (height > 540 && height < 1080) {
+		slip = 3;
 	} else {
-		gridslip = 4;
+		slip = 4;
 	}
 
-	// Set the inline pixel step distance
-	px_step = SQUARE(gridslip) - 1;
+	// Set the inline pixel step
+	step = SQUARE(slip) - 1;
 
-	// Generate a random step offset (It helps to eliminate any remaining dirty zones between each image update.)
+	// Generate a random step shift (It helps to eliminate any remaining dirty zones between each image update.)
 	srand(time(NULL));
-	px_random = rand() % px_step;
+	shift = rand() % step;
 
 	// Compare the buffers and find the differences in every line
-	for (j = 0; j < vncscr->height; j++) {
+	for (y = 0; y < height; y++) {
 
 		// Set all offsets
-		vb_offset = j * vncscr->width;
-		fb_offset = (j + scrinfo.yoffset) * scrinfo.xres_virtual + scrinfo.xoffset;
-		px_offset = (j * gridslip + px_random) % px_step;
+		vb_offset = y * width;
+		fb_offset = (y + scrinfo.yoffset) * scrinfo.xres_virtual + scrinfo.xoffset;
+		px_offset = (y * slip + shift) % step;
 
-		// Compare certain pixels in this line with an offset
-		for (i = px_offset; i < vncscr->width; i += px_step) {
-			if (vb[i + vb_offset] != fb[i + fb_offset]) {
+		// Compare certain pixels in every line with an offset
+		for (x = px_offset; x < width; x += step) {
+			if (vb[x + vb_offset] != fb[x + fb_offset]) {
 				if (idle) {
+					// The current line reduced by the slip value -> Set as the first different line
+					min_y = MIN(y - slip, min_y);
 					idle = 0;
-					min_y = MIN(j - gridslip, min_y); // Set the current line as the first
 				}
-
-				// Set the current line as the last
-				max_y = MAX(j + gridslip, max_y);
+				// The current line increased by the slip value -> Set as the last different line
+				max_y = MAX(y + slip, max_y);
 				break; // There is no need to examine this line anymore if it already has a difference
 			}
 		}
 	}
 
+	// Fill the image buffer with the new content
 	if (!idle) {
 		min_x = 0;
 		min_y = MAX(0, min_y);
-		max_x = screenformat.width - 1;
-		max_y = MIN(screenformat.height - 1, max_y);
+		max_x = width - 1;
+		max_y = MIN(height - 1, max_y);
 
-		// Fill the image buffer with the new content
-		for (j = min_y; j <= max_y; j++) {
-			vb_offset = j * vncscr->width;
-			fb_offset = (j + scrinfo.yoffset) * scrinfo.xres_virtual + scrinfo.xoffset;
+		for (y = min_y; y <= max_y; y++) {
+			vb_offset = y * width;
+			fb_offset = (y + scrinfo.yoffset) * scrinfo.xres_virtual + scrinfo.xoffset;
 			memcpy(vncbuf + vb_offset, fb + fb_offset, screenformat.width * screenformat.bitsPerPixel / CHAR_BIT);
 		}
 
